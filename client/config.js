106918986c75d28a545a1ac01df1eb6a13bdcdc9
AnShAwAsThY10 <anshawasthy1234@gmail.com>
2026-03-19 16:46:31 +0530

    feat: Update API_BASE_URL for Google Apps Script Web App

--- client/config.js
+++ client/config.js
@@ -2,4 +2,4 @@
 // Google Apps Script Web App URL
 // Paste the URL you get after deploying Code.gs
 // ============================================
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbx7yXjds90pSUhskVilEWWymWLykEhKN0HELVnR1xrihKEQknEhV_ZZAa0AtkENi9_U6Q/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzHe0e-RYsds93WXriBroHzWiusxQdF3ycNdjlAuaslRyjKJco27wSOOjSe5c8x33BeBg/exec';
